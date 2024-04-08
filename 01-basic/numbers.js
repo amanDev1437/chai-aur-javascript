194839958f8a8a9b62ed4f1929660e5ec999f0a1
amanDev1437 <amanpratap1437@gmail.com>
2024-04-08 15:32:18 +0530

    number

---
+++ 01-basic/numbers.js
@@ -0,0 +1,7 @@
+const balance = Number(100);
+
+const numberToString = balance.toString();
+console.log(typeof(numberToString));
+
+const dice = Math.floor((Math.random()*10) + 1);
+console.log(dice);
